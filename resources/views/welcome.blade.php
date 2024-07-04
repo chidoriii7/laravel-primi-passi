ed6514563d33f6db0fe99ff29de52e921754d3f9
chidoriii7 <muratore.danilo@yahoo.it>
2024-07-04 20:18:17 +0200

    laravel-primi-passi: hello world

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <div>
+        <h1>Hello World with Laravel</h1>
+        <h2>Hello World with Laravel</h2>
+        <h3>Hello World with Laravel</h3>
+        <h4>Hello World with Laravel</h4>
+    </div>
+</body>
+</html>
